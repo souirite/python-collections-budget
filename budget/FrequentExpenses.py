38e446d359ccd96e0a35e4d249f972b650dd918b
Adil <adil.souirite@gmail.com>
2023-02-28 11:39:12 +0000

    Update FrequentExpenses.py

---
+++ budget/FrequentExpenses.py
@@ -0,0 +1,23 @@
+from . import Expense
+import collections
+import matplotlib.pyplot as plt
+
+expences = Expense.Expenses()
+expences.read_expenses("data/spending_data.csv")
+
+spending_categories = []
+
+for expence in expences.list:    
+    spending_categories.append(expence.category)
+
+spending_counter = collections.Counter(spending_categories)
+top5 = spending_counter.most_common(5)
+
+categories, count = zip(*top5)
+
+fig, ax = plt.subplots()
+
+ax.bar(categories, count)
+ax.set_title('# of Purchases by Category')
+
+plt.show()
